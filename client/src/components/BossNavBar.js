effbe81997e6b72127037fbb789cf836571f131d
beatriz <beatrizffeo@gmail.com>
2024-06-20 23:48:04 -0600

    navbar

--- client/src/components/BossNavBar.js
+++ client/src/components/BossNavBar.js
@@ -4,11 +4,17 @@ import Logout from "./Logout";
 import "../NavBar.css";
 
 function BossNavBar({ logout }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <img src="../logoDNF.png" alt="Company Logo" className="nav-logo" />
 
-      <div className="nav-links-container">
+      <div className={`nav-links-container ${isOpen ? "open" : ""}`}>
         <NavLink to="/home" exact className="nav-link" activeClassName="active">
           <button>Home</button>
         </NavLink>
@@ -24,8 +30,13 @@ function BossNavBar({ logout }) {
         <NavLink to="/worklogs" className="nav-link" activeClassName="active">
           <button>Worklogs</button>
         </NavLink>
-
         <Logout logout={logout} />
+      </div>
+
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </nav>
   );
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logout from "./Logout";
 import "../NavBar.css";
 
 function NavBar({ logout, user }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <img src="../logoDNF.png" alt="Company Logo" className="nav-logo" />
-      <div className="nav-links-container">
+      <div className={`nav-links-container ${isOpen ? "open" : ""}`}>
         <NavLink
           to="/home/employee"
           exact
@@ -16,12 +22,15 @@ function NavBar({ logout, user }) {
         >
           <button className="nav-link button">Home</button>
         </NavLink>
-        {
-          <NavLink to={`/employee/${user.id}`}>
-            <button className="nav-link button">My profile</button>
-          </NavLink>
-        }
+        <NavLink to={`/employee/${user.id}`} className="nav-link">
+          <button className="nav-link button">My Profile</button>
+        </NavLink>
         <Logout logout={logout} />
+      </div>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </nav>
   );
